c2d86075c8c9e7a36d73879e1d99da56607a245e
Muhammad Wahab Sharif <www.rex19@gmail.com>
2024-05-06 12:45:57 +0500

    cmt

--- src/components/Index/Awards.js
+++ src/components/Index/Awards.js
@@ -31,26 +31,42 @@ const options = {
 
 const { tagline, title, projects } = awardsData;
 
+/**
+ * Renders the Awards component.
+ *
+ * @return {JSX.Element} The rendered Awards component.
+ */
 const Awards = () => {
+  // Define the Awards component that renders a section with a list of awards and achievements.
   return (
+    // Start the section with the class "project-six"
     <section className="project-six">
       <div className="auto-container">
+        {/* Start the section's title */}
         <div className="sec-title-six text-center">
+          {/* Display the tagline for the awards */}
           <p className="sec-title-six__text">
             <span>{tagline}</span>
           </p>
+          {/* Display the title of the awards */}
           <h2 className="sec-title-six__title">{title}</h2>
         </div>
 
+        {/* Start the Swiper component for the awards slideshow */}
         <Swiper {...options} className="thm-swiper__slider">
+          {/* Start the swiper wrapper */}
           <div className="swiper-wrapper">
+            {/* Map over the list of projects and render each one as a SwiperSlide */}
             {projects.map(({ id, image, category, title }) => (
               <SwiperSlide key={id}>
+                {/* Start a project item */}
                 <div className="project-six__item">
+                  {/* Render an image from the awards directory */}
                   <Image
                     src={require(`@/images/awards/${image}`).default.src}
                     alt=""
                   />
+                  {/* Display the category and title of the award */}
                   <div className="project-six__content">
                     <p className="project-six__category">{category}</p>
                     <h3 className="project-six__title">
