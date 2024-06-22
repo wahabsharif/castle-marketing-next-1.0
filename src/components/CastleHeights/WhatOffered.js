11f82699a8c19d279341b2d4e2fa00b6a541fd1a
wahab <www.rex19@gmail.com>
2024-06-22 15:10:00 +0500

    Castle Heights: Whats Offered

--- src/components/CastleHeights/WhatOffered.js
+++ src/components/CastleHeights/WhatOffered.js
@@ -1,5 +1,3 @@
-// WhatOffered.js
-
 import React, { useState, useEffect } from "react";
 import { whatOffered, circleData } from "@/data/CastleHeights/whatOffered";
 import styles from "@/styles/WhatOffered.module.css"; // Import CSS module for styles
@@ -16,6 +14,11 @@ const WhatOffered = () => {
   useEffect(() => {
     setSelectedCategory("shops");
   }, []); // Empty dependency array ensures this runs only once on mount
+
+  // Function to find data object for selected category
+  const getCategoryData = (category) => {
+    return whatOffered.find((item) => item.category === category);
+  };
 
   return (
     <section>
@@ -37,73 +40,77 @@ const WhatOffered = () => {
       <div className={styles["what-offered-container"]}>
         <div className={styles["button-list"]}>
           <ul>
-            <li>
-              <button
-                className={selectedCategory === "shops" ? styles["active"] : ""}
-                onClick={() => handleClick("shops")}
-              >
-                Shops
-              </button>
-            </li>
-            <li>
-              <button
-                className={
-                  selectedCategory === "apartments" ? styles["active"] : ""
-                }
-                onClick={() => handleClick("apartments")}
-              >
-                Apartments
-              </button>
-            </li>
-            <li>
-              <button
-                className={
-                  selectedCategory === "offices" ? styles["active"] : ""
-                }
-                onClick={() => handleClick("offices")}
-              >
-                Offices
-              </button>
-            </li>
-            <li>
-              <button
-                className={
-                  selectedCategory === "penthouses" ? styles["active"] : ""
-                }
-                onClick={() => handleClick("penthouses")}
-              >
-                Penthouses
-              </button>
-            </li>
+            {whatOffered.map((item) => (
+              <li key={item.id}>
+                <button
+                  className={
+                    selectedCategory === item.category ? styles["active"] : ""
+                  }
+                  onClick={() => handleClick(item.category)}
+                >
+                  {item.title}
+                </button>
+              </li>
+            ))}
           </ul>
         </div>
         <div className={styles["content"]}>
           {selectedCategory && (
             <div className={styles["card"]}>
-              <Image
-                src={
-                  whatOffered.find((item) => item.category === selectedCategory)
-                    .imgSrc
-                }
-                alt={selectedCategory}
-                width={800} // Adjust width as needed
-                height={600} // Adjust height as needed
-              />
               <div className={styles["card-text"]}>
-                <h2>
-                  {
-                    whatOffered.find(
-                      (item) => item.category === selectedCategory
-                    ).title
-                  }
-                </h2>
-                <p>
-                  {
-                    whatOffered.find(
-                      (item) => item.category === selectedCategory
-                    ).description
-                  }
-                </p>
+                <div className={styles["title"]}>
+                  <Image
+                    src={
+                      require(`@/images/castle-heights/${
+                        getCategoryData(selectedCategory)?.icon
+                      }`).default.src
+                    }
+                    alt=""
+                    width={0}
+                    height={0}
+                  />
+                  <h2>{getCategoryData(selectedCategory)?.title}</h2>
+                </div>
+
+                <p>{getCategoryData(selectedCategory)?.description}</p>
+                {getCategoryData(selectedCategory)?.unitType && (
+                  <p>
+                    <strong>Unit Type:</strong>{" "}
+                    {getCategoryData(selectedCategory)?.unitType}
+                  </p>
+                )}
+                {getCategoryData(selectedCategory)?.area && (
+                  <p>
+                    <strong>Area:</strong>{" "}
+                    {getCategoryData(selectedCategory)?.area}
+                  </p>
+                )}
+                {getCategoryData(selectedCategory)?.bookingStartsFrom && (
+                  <p>
+                    <strong>Booking Starts From:</strong>{" "}
+                    {getCategoryData(selectedCategory)?.bookingStartsFrom}
+                  </p>
+                )}
+                {getCategoryData(selectedCategory)?.availableApartments && (
+                  <p>
+                    <strong>Available Apartments:</strong>{" "}
+                    {getCategoryData(selectedCategory)?.availableApartments}
+                  </p>
+                )}
+                {getCategoryData(selectedCategory)?.paymentPlanType && (
+                  <p>
+                    <strong>Payment Plan Type:</strong>{" "}
+                    {getCategoryData(selectedCategory)?.paymentPlanType}
+                  </p>
+                )}
+              </div>
+              <div className={styles["image-container"]}>
+                <Image
+                  src={getCategoryData(selectedCategory)?.imgSrc}
+                  alt={selectedCategory}
+                  width={800} // Adjust width as needed
+                  height={600} // Adjust height as needed
+                />
               </div>
             </div>
           )}
