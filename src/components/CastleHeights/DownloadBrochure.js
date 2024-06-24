6efc8ac701f613899e34f785fa5328e03afef148
wahab <www.rex19@gmail.com>
2024-06-24 17:35:00 +0500

    Castle Hights: Ups

--- src/components/CastleHeights/DownloadBrochure.js
+++ src/components/CastleHeights/DownloadBrochure.js
@@ -1,11 +1,13 @@
 import React from "react";
 import styles from "@/styles/DownloadBrochure.module.css";
+import Image from "next/image";
+import CMProfileBanner from "@/images/castle-heights/castle-prifile-banner.png";
 
 const DownloadBrochure = () => {
   const handleDownload = () => {
     const link = document.createElement("a");
-    link.href = "/pdf/castle-marketing-profile.pdf";
-    link.download = "Castle Heights Brochure.pdf";
+    link.href = "/pdf/castle-heights-profile.pdf";
+    link.download = "Castle Heights Profile.pdf";
     document.body.appendChild(link);
     link.click();
     document.body.removeChild(link);
@@ -15,13 +17,21 @@ const DownloadBrochure = () => {
     <div className="auto-container">
       <div className={styles.secTitle}>
         <h2>
-          Castle Download Brochure
+          Castle Heights Profile
           <span className="dot">.</span>
         </h2>
       </div>
+      <div className={styles["image-container"]}>
+        <Image
+          src={CMProfileBanner}
+          alt="Castle Marketing Profile"
+          width={1600} // Adjust width as needed
+          height={1200} // Adjust height as needed
+        />
+      </div>
       <div className={styles.centerContainer}>
         <button onClick={handleDownload} className={styles.downloadButton}>
-          Download Brochure
+          See Full Profile
         </button>
       </div>
     </div>
